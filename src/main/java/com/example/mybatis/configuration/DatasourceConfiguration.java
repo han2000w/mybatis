69bce5f155d4985c6dd2a47ed719075210e51758
Your Name <you@example.com>
2021-11-22 17:00:12 +0900

    first commit

--- src/main/java/com/example/mybatis/configuration/DatasourceConfiguration.java
+++ src/main/java/com/example/mybatis/configuration/DatasourceConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.mybatis.configuration;
 
+import com.example.mybatis.DataSourceMonitoring;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import javax.annotation.PostConstruct;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 import javax.sql.DataSource;
+import java.lang.management.ManagementFactory;
+
 @Configuration
 public class DatasourceConfiguration {
 
@@ -19,4 +26,16 @@ public class DatasourceConfiguration {
 
         return sessionFactory.getObject();
     }
+
+    private HikariDataSource dataSource;
+
+    @PostConstruct
+    public void init() throws Exception {
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        ObjectName objectName = new ObjectName("com.test:type=DataSource,name=hikari-pool,context=/");
+        DataSourceMonitoring dataSourceMonitoring = new DataSourceMonitoring(dataSource);
+        mBeanServer.registerMBean(dataSourceMonitoring, objectName);
+    }
 }
+
+
